0f4fda4d1e548b8aa9a72967c9699dba85abcfb6
Zahra Shamsi <zshamsi2@illinois.edu>
2017-08-10 16:52:17 -0500

    Update and rename CC-interactiveJob to CC-interactiveJob.sh

--- CC-interactiveJob.sh
+++ CC-interactiveJob.sh
@@ -1,4 +1,4 @@
-# interactive job on campuscluster ?!! not working
+# interactive job on campuscluster 
 qsub -q cse -I -l walltime=00:30:00,nodes=1:ppn=12 mkCluster.py 
 # size of directory
 du -hcs ANC-S1/MSM/
